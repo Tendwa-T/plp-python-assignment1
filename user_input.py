14526af20b0d606506eb6efaba949f96dd38fd96
Tendwa-T <tirus301@outlook.com>
2024-03-01 00:33:22 +0300

    Week 1 assignment

---
+++ user_input.py
@@ -0,0 +1,10 @@
+print("Hello User!!")
+
+def user_input():
+    name = input("Enter your name: ")
+    age = input("Enter your age: ")
+    location = input("Enter your location: ")
+
+    print(f"Hello {name}. How is age {age} taking you. Glad to see you here in {location}")
+
+user_input()
